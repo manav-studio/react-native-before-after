0852a15ecc944cbf23fd87fb5ae48f10e72245b6
MD MOHIBUR RAHMAN <mrpmohiburrahman@gmail.com>
2022-10-03 20:03:39 +0600

    add example repo with example app

---
+++ example/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'white' }}>
+      <Text>Hello world</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
